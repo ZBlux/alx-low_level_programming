31deb0e160a7edad25e6e716956c75d0007f601e
ZBlux <aminekamili@hotmail.com>
2023-06-15 12:16:06 -0700

    using pritf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function is the entry point of the program.
+ *              The program prints a message followed by new line.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
