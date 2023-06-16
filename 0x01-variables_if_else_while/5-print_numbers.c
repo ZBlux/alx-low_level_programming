a12a294945c9c87654fe26f4e2d62255f3a77879
ZBlux <aminekamili@hotmail.com>
2023-06-16 14:01:16 -0700

    zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main- the entry point
+ *
+ *Description: from zero to nine
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num == 0; num <= 9; num++)
+	{
+		printf("%d", cum);
+	}
+	printf("\n");
+	return (0);
+}
