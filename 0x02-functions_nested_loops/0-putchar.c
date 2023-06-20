2076d567b2698f7640fef17cbae6f3ba883c1f5c
ZBlux <aminekamili@hotmail.com>
2023-06-20 14:30:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ *main- the entry point.
+ *
+ *Description: Prints _putchar.
+ *
+ *Return: 0 .
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[8]);
+	_putchar('\n');
+	}
+	return (0);
+}
