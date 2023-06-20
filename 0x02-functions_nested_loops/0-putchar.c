b0f7aff88c0ef215c630898013cacd129c5cdf0b
ZBlux <aminekamili@hotmail.com>
2023-06-20 14:37:41 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  *main- the entry point.
  *
@@ -13,8 +13,8 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(c[8]);
-	_putchar('\n');
+		_putchar(c[i]);
+		_putchar('\n');
 	}
 	return (0);
 }
