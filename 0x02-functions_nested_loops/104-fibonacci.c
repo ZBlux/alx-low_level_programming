9fb83feea250f4b6ddb4b29be9144a72158e4664
ZBlux <aminekamili@hotmail.com>
2023-06-21 13:02:50 -0700

    fibonacci rabbit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main- the entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 2, fn;
+	int c;
+
+	printf("%lu, %lu", f1, f2);
+
+	for (c = 3; c <= 96; c++)
+	{
+		fn = f1 + f2;
+		printf("%lu", fn);
+		f1 = f2;
+		f2 = fn;
+	}
+	printf('\n');
+	return (0);
+}
