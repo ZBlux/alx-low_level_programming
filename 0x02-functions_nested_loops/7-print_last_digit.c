78b87029924c6fc346a74fd9357e146d975bf3b8
ZBlux <aminekamili@hotmail.com>
2023-06-20 17:38:46 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_last_digit- function that prints last digit
+ *
+ *@n: number
+ *
+ *Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int ls;
+
+	ls =  n % 10;
+	if (ls < 0)
+		ls = -ls;
+		_putchar('0' + ls);
+		return (ls);
+}
