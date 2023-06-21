48d76e57eaeff3fbe1e0ed91fd0c5164da706a2e
ZBlux <aminekamili@hotmail.com>
2023-06-20 18:28:50 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *times_table- jadwal darb
+ *
+ */
+void times_table(void)
+{
+	int i, f, p;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (f = 0; f <= 9; f++)
+		{
+			p = i * f;
+			if (i != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (p <= 9 && f != 0)
+				_putchar(' ');
+			if (p >= 10)
+			{
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
