671da48a8a2ecd2ba28d9c7722dd8f9c70d424de
ZBlux <aminekamili@hotmail.com>
2023-06-20 17:02:48 -0700

    checks sign for numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign- checks sign.
+ *
+ *@n: the number to check.
+ *
+ *Return: 1 if pos, -1 if neg , 0 if 0 .
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar("+");
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar("-");
+	}
+	else
+	{
+		return (0);
+		_putchar("0");
+	}
+}
