9f09c26ac2409a5e9cc313e5b7bae8dda6983422
ZBlux <aminekamili@hotmail.com>
2023-06-22 05:59:01 -0700

    without 4 and 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,12 @@
  */
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (c != '4' && c != '2')
-		_putchar(c);
+		if (i != '4' && i != '2')
+		_putchar(i);
 	}
 	_putchar('\n');
 }
