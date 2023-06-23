4d34f6751f32c955254293cd88319a3782719cbb
ZBlux <aminekamili@hotmail.com>
2023-06-22 17:34:03 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,8 +10,10 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) && (i % 5)) == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
 			printf("FizzBuzz");
+		}
 
 		else if ((i % 3) == 0)
 			printf("Fizz");
@@ -24,8 +26,6 @@ int main(void)
 		if (i != 100)
 			printf(" ");
 	}
-
 	printf("\n");
-
 	return (0);
 }
