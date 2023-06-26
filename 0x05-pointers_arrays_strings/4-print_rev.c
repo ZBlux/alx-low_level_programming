7404e0fc6e780ca53a86dc6f228fda37b54be6c4
ZBlux <aminekamili@hotmail.com>
2023-06-26 13:30:13 -0700

    reverse my string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,28 @@
 #include "main.h"
 
 /**
+ *_strlen- lenght of string
+ *
+ *@s: string
+ *
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
  *print_rev- prints in reverse
  *
  *@s: string
+ *
  */
 
 void print_rev(char *s)
