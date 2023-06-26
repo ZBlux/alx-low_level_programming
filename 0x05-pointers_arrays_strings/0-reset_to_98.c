f846a0e083fc65fc4516b40c818a3e6ff6d8c290
ZBlux <aminekamili@hotmail.com>
2023-06-26 09:38:18 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98- function that resets to 98
+ *
+ *@*n: variable
+ */
+void reset_to_98(int *n)
+{
+	int *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+now pointers, arrays and strings
