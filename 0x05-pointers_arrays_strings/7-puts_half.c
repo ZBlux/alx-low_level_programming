4c4518c36e30e0a2c9f9aef28ea2e9f69d25e2e9
ZBlux <aminekamili@hotmail.com>
2023-06-26 14:42:46 -0700

    prints half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *_strlen- lenght of string
+ *
+ *@s: string
+ *
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ *puts_half- prints half
+ *
+ *@str: string
+ */
+
+void puts_half(char *str)
+{
+	j = _strlen(str) / 2;
+
+	for (j = _strlen(str) / 2; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
