c6c172ac1079152f8d64494b49e57a986035d9ef
ZBlux <aminekamili@hotmail.com>
2023-06-26 13:14:42 -0700

    reverse my string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
+ *print_rev- prints in reverse
  *
- *
- *
+ *@s: string
  */
 
 void print_rev(char *s)
 {
+	int i;
+	int j;
+
+	j = _strlen(s);
+	for (i = 0; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
 
 }
