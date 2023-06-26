6ddc4a43b220e4de8600486be3d2143a721ee664
ZBlux <aminekamili@hotmail.com>
2023-06-26 13:37:03 -0700

    weird

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *_strlen- lenght of string
+ *
+ *@s: string
+ *
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ *print_rev- prints in reverse
+ *
+ *@s: string
+ *
+ */
+
+void print_rev(char *s)
+{
+	int j;
+
+	for (j = _strlen(s) - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
