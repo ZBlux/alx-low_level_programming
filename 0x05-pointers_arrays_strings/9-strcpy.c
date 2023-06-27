bf84a855d607a72259e09ec8acc75b0605fe1359
ZBlux <aminekamili@hotmail.com>
2023-06-26 19:17:08 -0700

    my head hurts

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@
 char *_strcpy(char *dest, char *src)
 {
         int i;
+	int j;
         for (i = 0; src[i] != '\0'; i++) {
                 dest[i] = src[i];
         }
-        int j;
         for (j = i; dest[j] != '\0'; j++) {
                 dest[j] = '\0';
         }
