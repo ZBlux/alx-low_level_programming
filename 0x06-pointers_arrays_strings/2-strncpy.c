9b6c25186563d422bd5df81a29fce08f51bfc19a
ZBlux <aminekamili@hotmail.com>
2023-06-29 14:54:09 -0700

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strncpy- function that copies a string
+ *
+ *@dest: destination
+ *@src: source
+ *@n: n bytes
+ *
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] = '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
