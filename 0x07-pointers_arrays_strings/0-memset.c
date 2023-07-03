c9b9150226a26c428eaa47af3f9c94d8a284712d
ZBlux <aminekamili@hotmail.com>
2023-07-03 07:59:59 -0700

    memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_memset- function that fills memory with constant
+ *
+ *@s: pointer to the memory
+ *@b: constant byte
+ *@n: fist bytes
+ *
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
