7bb5c680f9087135cb96eba49dbf44657cfb7cdb
ZBlux <aminekamili@hotmail.com>
2023-07-03 13:51:59 -0700

    locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, k;
 
-	for (i = 0; haystackhaystack[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		k = i;
 		for (j = 0; needle[j] != '\0' && haystack[k] != '\0'; j++)
