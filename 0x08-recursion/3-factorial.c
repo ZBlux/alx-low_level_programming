a1fa9c6b81d4f1e91616f2d5ad2292bda5066fcd
ZBlux <aminekamili@hotmail.com>
2023-07-05 13:14:13 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial- function that return factorial of n
+ *
+ * @n: int
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
