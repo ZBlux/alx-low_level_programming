85a73117a629530b0db030ec63cf1bc66e882ce9
ZBlux <aminekamili@hotmail.com>
2023-07-07 15:18:44 -0700

    maybe fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	if (*argv)
-	{
-		printf("%d\n", argc - 1);
-	}
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return 0;
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main- add numbers
+ *
+ *@argc: number of arguments
+ *@argv: array name
+ *
+ *Return: result or 0 if no number or 1 if Error
+ */
+int main(int argc, char *argv[])
+{
+	int i, result;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (
+}
