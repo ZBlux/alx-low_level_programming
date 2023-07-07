60b522202e509b1ad2e365e68feecffe5e66cbbb
ZBlux <aminekamili@hotmail.com>
2023-07-07 11:33:52 -0700

    print the name file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- prints name file
+ *
+ *@argc: count the arguments
+ *@argv: strings of argument
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[0]);
+	return (0);
+}
