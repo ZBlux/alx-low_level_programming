bfff1845f6daa5fc9ba41d23b028987e557e0105
ZBlux <aminekamili@hotmail.com>
2023-07-10 11:46:21 -0700

    string copy with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_strdup- copy a string
+ *
+ *@str: string
+ *
+ *Return: the copy or NULL
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0, str[i] != '\0', i++)
+	{
+	}
+	p = mallpc(sizeof(char) * (i + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j <= i; j++)
+	{
+		p[j] = str[j];
+	}
+	return (p);
+}
