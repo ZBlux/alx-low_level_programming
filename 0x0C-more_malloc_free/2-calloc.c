30315c4dfc51374a5a7e84ba9f11dce99a728a65
ZBlux <aminekamili@hotmail.com>
2023-07-13 17:54:08 -0700

    callocs function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+
+/**
+ *_memset- function that fills memory with constant
+ *
+ *@s: pointer to the memory
+ *@b: constant byte
+ *@n: fist bytes
+ *
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: the number of elements
+ * @size: the size
+ * Return: p or NULL 
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	__memset(p, 0, nmemb * size);
+	return (p);
+}
