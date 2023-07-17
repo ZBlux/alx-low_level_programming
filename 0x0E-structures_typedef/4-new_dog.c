1c7e9a8da9919aad187f4bc55a8fe23ff01d9632
ZBlux <aminekamili@hotmail.com>
2023-07-17 15:07:22 -0700

    free the dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
- * free_dog - a functions that frees a dog
- * @d: the dog
+ *
+ *
+ *
+ *
+ *
  */
 
-void free_dog(dog_t *d)
-{
-	if (d == NULL)
-	{
-		return;
-	}
-	free(d->name);
-	free(d->owner);
-	free(d);
-}
