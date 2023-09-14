d73e9bc1d6d248043fa394c7e8aea62415af52db
ZBLUX <aminekamili@hotmail.com>
2023-09-14 13:53:31 -0700

    easy 2 tasks

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *print_dlistint- function that prints all elements
+ *
+ *@h: head
+ *
+ *Return: n
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
